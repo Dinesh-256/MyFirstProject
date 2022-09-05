76d5ca30352c997e9d345fb190e5f23f8e327c6d
intel-vsc <intel-vsc@DESKTOP-K78MAGB>
2022-09-05 12:47:40 +0530

    "first commit"

---
+++ src/arraylist/Sysout.java
@@ -0,0 +1,21 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sysout {
+ public static void main(String[] args) {
+//		List<Integer> list = new ArrayList<Integer>() {{
+//            add(1);
+//            add(3);
+//            } };
+//        System.out.println("ArrayList : " + list);
+//	}
+	
+	List<Integer> list=Arrays.asList(1, 2, 3);{
+	System.out.println(list);
+	}
+	}
+
+}
